1502235991b10adb4bfa5cf99a03664cef4e99e3
Yoshiaki Kawazu <kawazzz@gmail.com>
2022-12-09 11:46:19 +0900

    テストコードを修正

--- main.ts
+++ main.ts
@@ -1,18 +1,1 @@
-let strip: neopixel.Strip = null
-let c = 0
-function 筑波ランタン色コードの確認 () {
-    strip = neopixel.create(DigitalPin.P1, 16, NeoPixelMode.RGB)
-    c = 0
-    for (let index = 0; index < 16; index++) {
-        if (7 < c) {
-            c = 0
-        } else {
-            if (c < 0) {
-                c = 0
-            }
-        }
-        strip.showColor(TukubaLantern.colorMap[c])
-        basic.showNumber(c)
-        c += 1
-    }
-}
+// 筑波ランタン.色コード確認()
--- test.ts
+++ test.ts
@@ -31,3 +31,27 @@
 // })
 // let strip: neopixel.Strip = null
 // TukubaLantern.lanternInit("1", 300, 200)
+
+
+namespace 筑波ランタン {
+    //筑波ランタン.色コード確認()
+
+    let strip: neopixel.Strip = null
+    let c = 0
+    export function 色コード確認() {
+        strip = neopixel.create(DigitalPin.P1, 16, NeoPixelMode.RGB)
+        c = 0
+        for (let index = 0; index < 16; index++) {
+            if (7 < c) {
+                c = 0
+            } else {
+                if (c < 0) {
+                    c = 0
+                }
+            }
+            strip.showColor(TukubaLantern.colorMap[c])
+            basic.showNumber(c)
+            c += 1
+        }
+    }
+}
